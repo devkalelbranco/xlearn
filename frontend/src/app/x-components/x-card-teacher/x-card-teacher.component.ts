0e4e0425383ca046969481d2233df08a1f618328
devkalelbranco <dev.kalel.branco@gmail.com>
2023-09-22 19:30:14 -0300

    Feat: free class badge

--- frontend/src/app/x-components/x-card-teacher/x-card-teacher.component.ts
+++ frontend/src/app/x-components/x-card-teacher/x-card-teacher.component.ts
@@ -9,6 +9,7 @@ export class XCardTeacherComponent {
   @Input('image') image:string="assets/persons/teacher_default.jpg";
   @Input('value') value?:string;
   @Input('rate') rate:number = 0;
+  @Input('free-class') freeClass:number = 0;
   @Input('text') text?:string;
   @Input('title') title?:string;
   @Input('footer') footer?:string;
